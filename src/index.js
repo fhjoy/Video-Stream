e2154f31fe224adf26d0697805b64098e357fad5
Md Faisal Hossain <faisalh.iu@gmail.com>
2021-10-28 00:29:02 +0200

    added google Auth

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import reactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import App from './components/App';
+import reducers from './reducers';
 
-reactDom.render(<App />, document.querySelector('#root'));
+const store = createStore(reducers);
+
+reactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
