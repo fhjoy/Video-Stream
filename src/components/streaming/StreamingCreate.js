2b76004261e9cfb9d4e79a2598e60a2522be29fd
Md Faisal Hossain <faisalh.iu@gmail.com>
2021-11-01 11:01:42 +0100

    added redux form

--- src/components/streaming/StreamingCreate.js
+++ src/components/streaming/StreamingCreate.js
@@ -1,5 +1,60 @@
-const StreamingCreate = () => {
-  return <div> StreamingCreate </div>;
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class StreamingCreate extends React.Component {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header"> {error} </div>
+        </div>
+      );
+    }
+  }
+
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
+    return (
+      <div className={className}>
+        <label> {label} </label>
+        <input {...input} />
+        {this.renderError(meta)}
+      </div>
+    );
+  };
+
+  onSubmit(formValues) {}
+
+  render() {
+    return (
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form error"
+      >
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
+        <button className="ui button primary"> Publish </button>
+      </form>
+    );
+  }
+}
+
+const validate = formValues => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = 'Please Enter Title';
+  }
+  if (!formValues.description) {
+    errors.description = 'Please Enter Description';
+  }
+  return errors;
 };
 
-export default StreamingCreate;
+export default reduxForm({
+  form: 'StreamingCreate',
+  validate,
+})(StreamingCreate);
