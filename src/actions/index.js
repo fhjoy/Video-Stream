b83074b1d57765be6b6259fbabd1bf4d03cbc9eb
Md Faisal Hossain <faisalh.iu@gmail.com>
2021-11-03 08:14:10 +0100

    added action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,14 @@
 import { formValues } from 'redux-form';
 import streams from './apis/streams';
-import { SIGN_IN, SIGN_OUT } from './type';
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM,
+} from './type';
 
 export const signIn = userId => {
   return {
@@ -16,5 +24,31 @@ export const signOut = () => {
 };
 
 export const createStream = formValues => async dispatch => {
-  streams.post('/streams', formValues);
+  const response = await streams.post('/streams', formValues);
+
+  dispatch({ type: CREATE_STREAM, payload: response.data });
+};
+
+export const fetchStreams = () => async dispatch => {
+  const response = await streams.get('/streams', formValues);
+
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+export const fetchStream = id => async dispatch => {
+  const response = await streams.get(`/streams/${id}`);
+
+  dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+  const response = await streams.put(`/streams/${id}`, formValues);
+
+  dispatch({ type: EDIT_STREAM, payload: response.data });
+};
+
+export const deleteStream = id => async dispatch => {
+  await streams.delete(`/streams/${id}`);
+
+  dispatch({ type: DELETE_STREAM, payload: id });
 };
